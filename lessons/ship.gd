1aa36c1813effce2e0675e6881b07c0a2c417822
seremo17 <seremo@s.sfusd.edu>
2025-12-04 14:12:04 -0800

    M05-L9 Completed
    M05-L9 Completed

--- lessons/ship.gd
+++ lessons/ship.gd
@@ -17,7 +17,10 @@ func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
-
+	var viewport_size := get_viewport_rect().size
+	position.x = wrapf(position.x, 0, viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
+	
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 
