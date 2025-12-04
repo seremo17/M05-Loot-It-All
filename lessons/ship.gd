f603acce7af3af09345b323fdf93291a2491545c
seremo17 <seremo@s.sfusd.edu>
2025-12-04 13:26:33 -0800

    M05-L7 Completed
    M05-L7 Completed

--- lessons/ship.gd
+++ lessons/ship.gd
@@ -1,9 +1,16 @@
-extends Sprite2D
-
+extends Area2D
 
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
+
+var health := 10
+var gem_count := 0
+
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	set_health(health)
 
 
 func _process(delta: float) -> void:
@@ -20,4 +27,21 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 
 	if velocity.length() > 0.0:
-		rotation = velocity.angle()
+		get_node("Sprite2D").rotation = velocity.angle()
+
+
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
+
+
+func set_health(new_health: int) -> void:
+	health = new_health
+	get_node("UI/HealthBar").value = health
+
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	if area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count + 1)
+	elif area_that_entered.is_in_group("healing_item"):
+		set_health(health + 10)
