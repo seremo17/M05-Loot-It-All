b1194cf06bbf1bb76b143e3811ef1a0a459fd073
seremo17 <seremo@s.sfusd.edu>
2025-12-05 12:46:39 -0800

    M05-L10 Completed
    M05-L10 Completed

--- lessons/health_pack.gd
+++ lessons/health_pack.gd
@@ -3,6 +3,20 @@ extends Area2D
 
 func _ready() ->void:
 	area_entered.connect(_on_area_entered)
+	play_floating_animation()
 	
 func _on_area_entered(area_that_entered:Area2D) ->void:
 	queue_free()
+
+
+func play_floating_animation() -> void:
+	pass
+	var tween := create_tween()
+	var sprite_2d := get_node("Sprite2D")
+	var position_offset := Vector2(0.0, 4.0)
+	var duration := 1.0
+	tween.tween_property(sprite_2d, "position", position_offset, duration)
+	tween.tween_property(sprite_2d, "position",  -1.0 * position_offset, duration)
+	tween.set_loops()
+	tween.set_trans(Tween.TRANS_SINE) 
+var duration = randf_range(0.8, 1.2)
